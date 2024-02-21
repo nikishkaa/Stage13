8f29284fcceb1e5dde281ec91c20baad3421c494
nikishkaa <nikita.goutor@gmail.com>
2024-02-21 16:02:19 +0300

    Update task 3. Add increasing and decreasing check.

--- src/by/itstep/goutor/javastage/stage13/task/task3/model/logic/NumbersLogic.java
+++ src/by/itstep/goutor/javastage/stage13/task/task3/model/logic/NumbersLogic.java
@@ -3,18 +3,52 @@ package by.itstep.goutor.javastage.stage13.task.task3.model.logic;
 public class NumbersLogic {
     public static boolean getOrderedNumbers(int[] numbers) {
 
-        boolean ordered = true;
+        boolean ordered = false;
 
-        for (int i = 1; i < numbers.length; i++) {
 
-            if (numbers[i] < numbers[i - 1]) {
-                ordered = false;
-                break;
-            }
+        boolean increasing = getIncreasing(numbers);
+        boolean decreasing = getDecreasing(numbers);
 
+        if (increasing || decreasing) {
+            ordered = true;
         }
 
 
         return ordered;
     }
+
+    private static boolean getIncreasing(int[] numbers) {
+
+        boolean increasing = true;
+
+
+        for (int i = 1; i < numbers.length; i++) {
+
+            if (numbers[i] < numbers[i - 1]) {
+                increasing = false;
+                break;
+            }
+
+        }
+
+        return increasing;
+    }
+
+
+    private static boolean getDecreasing(int[] numbers) {
+
+        boolean decreasing = true;
+        for (int i = 1; i < numbers.length; i++) {
+
+            if (numbers[i] > numbers[i - 1]) {
+                decreasing = false;
+                break;
+            }
+
+        }
+
+        return decreasing;
+    }
+
+
 }
